37fe6f4185cf78cea20cdd5aa5ad6bb340526140
Vikash Kumar <kvvik2020@gmail.com>
2024-05-02 16:41:56 +0530

    added the socket connection in client

---
+++ client/job-sheduler-fe/src/App.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    const newSocket = new WebSocket('ws://localhost:8080/api/v1/jobs');
+    newSocket.onopen = () => {
+      console.log('Connection established');
+      newSocket.send('Hello Server!');
+    }
+    newSocket.onmessage = (message) => {
+      console.log('Message received:', message.data);
+    }
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [])
+
+  return (
+    <>
+      hi there
+    </>
+  )
+}
+
+export default App
